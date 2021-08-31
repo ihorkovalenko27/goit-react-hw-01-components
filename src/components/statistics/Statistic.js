f2c4e967455a2bab64183b930d7e093d6a4df68a
Ihor Kovalenko <kovalyan27@gmail.com>
2021-08-31 21:54:46 +0300

    Fix title state

--- src/components/statistics/Statistic.js
+++ src/components/statistics/Statistic.js
@@ -13,7 +13,7 @@ function randomColor() {
 const Statistic = ({ title, stats }) => {
   return (
     <section className={s.statistics}>
-      <h2 className={s.title}>{title}</h2>
+      {title && <h2 className={s.title}>{title}</h2>}
 
       <ul className={s.statList}>
         {stats.map(stat => (
